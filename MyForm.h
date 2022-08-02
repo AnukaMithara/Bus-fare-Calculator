74e3e7103b842d8cb4404416269ef62496c9f56b
Anuka Mithara Karunanayaka <48363103+AnukaMithara@users.noreply.github.com>
2022-08-02 22:18:05 +0530

    Add files via upload

---
+++ MyForm.h
@@ -0,0 +1,244 @@
+﻿#pragma once
+
+namespace Assignment2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+		array<int, 2>^ twoDimensionalArray;
+
+	public:
+		MyForm(void)
+		{ 			InitializeComponent();
+			//Initialize array
+			this->twoDimensionalArray = (gcnew cli::array<int, 2> { {0, 0}, { 1, 66 }, { 2, 138 }, { 3, 196 }, { 4, 230 }, { 5, 285 }, { 6, 329 }, { 7, 393 }});
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::ComboBox^ comboBox2;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(71, 191);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(44, 18);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"From";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(71, 278);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(26, 18);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"To";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12));
+			this->label3->Location = System::Drawing::Point(22, 432);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(257, 25);
+			this->label3->TabIndex = 3;
+			this->label3->Text = L"Fare for this journey will be : ";
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(62, 331);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(135, 39);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"Calculate fare";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->Font = (gcnew System::Drawing::Font(L"Mongolian Baiti", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(8) {
+				L"Jaffna", L"Kilinochchi", L"Vavuniya", L"Anuradhapura",
+					L"Puttalam", L"Chilaw", L"Negombo", L"Colombo"
+			});
+			this->comboBox1->Location = System::Drawing::Point(186, 191);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(225, 26);
+			this->comboBox1->TabIndex = 5;
+			// 
+			// comboBox2
+			// 
+			this->comboBox2->Font = (gcnew System::Drawing::Font(L"Mongolian Baiti", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->comboBox2->FormattingEnabled = true;
+			this->comboBox2->Items->AddRange(gcnew cli::array< System::Object^  >(8) {
+				L"Jaffna", L"Kilinochchi", L"Vavuniya", L"Anuradhapura",
+					L"Puttalam", L"Chilaw", L"Negombo", L"Colombo"
+			});
+			this->comboBox2->Location = System::Drawing::Point(186, 271);
+			this->comboBox2->Name = L"comboBox2";
+			this->comboBox2->Size = System::Drawing::Size(225, 26);
+			this->comboBox2->TabIndex = 6;
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->Location = System::Drawing::Point(296, 331);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(149, 39);
+			this->button2->TabIndex = 7;
+			this->button2->Text = L"Reset";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12));
+			this->label4->Location = System::Drawing::Point(314, 432);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(0, 25);
+			this->label4->TabIndex = 8;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12));
+			this->label5->Location = System::Drawing::Point(352, 432);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(0, 25);
+			this->label5->TabIndex = 9;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(-8, 0);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(521, 170);
+			this->pictureBox1->TabIndex = 10;
+			this->pictureBox1->TabStop = false;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(515, 506);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->comboBox2);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->pictureBox1);
+			this->Name = L"MyForm";
+			this->Text = L"Jaffna -Colombo Bus fare Calculator";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		int C1 = (twoDimensionalArray[comboBox1->SelectedIndex, 1]); //selection of combo box 1
+		int C2 = (twoDimensionalArray[comboBox2->SelectedIndex, 1]); //selection of combo box 2
+		int fare;
+		//To get positive value
+		if (C1 > C2)
+		{
+			fare = (C1 - C2) * 2;
+
+		}
+		else
+		{
+			fare = (C2 - C1) * 2;
+		}
+		this->label4->Text = "Rs. ";
+		this->label4->ForeColor = System::Drawing::Color::Red;
+		this->label5->Text = Convert::ToString(fare);
+		this->label5->ForeColor = System::Drawing::Color::Red;
+
+		this->button1->Enabled = false;
+		this->button2->Enabled =true;
+
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->label4->Text = " ";
+		this->label5->Text = " ";
+		this->button1->Enabled = true;
+		this->button2->Enabled = false;
+}
+private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+};
+}
